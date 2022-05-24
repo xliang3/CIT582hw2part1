dcb2f598cec4414dd553bcacd8c2a156507ba0a9
Jstar414370153 <45889798+Jstar414370153@users.noreply.github.com>
2022-05-23 19:35:15 -0500

    initialize

---
+++ hash_collision.py
@@ -0,0 +1,20 @@
+import hashlib
+import os
+
+def hash_collision(k):
+    if not isinstance(k,int):
+        print( "hash_collision expects an integer" )
+        return( b'\x00',b'\x00' )
+    if k < 0:
+        print( "Specify a positive number of bits" )
+        return( b'\x00',b'\x00' )
+   
+    #Collision finding code goes here
+    x = b'\x00'
+    y = b'\x00'
+    
+    return( x, y )
+
+
+
+
