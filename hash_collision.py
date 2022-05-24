35f4d3d65639b1bf6be2c76ee7cbe97100ab9f5f
Jstar414370153 <45889798+Jstar414370153@users.noreply.github.com>
2022-05-23 19:59:10 -0500

    10th

--- hash_collision.py
+++ hash_collision.py
@@ -11,17 +11,43 @@ def hash_collision(k):
         return( b'\x00',b'\x00' )
    
     #Collision finding code goes here
+
+    def helper(code):
+        dic = {'0': '0000', 
+               '1': '0001', 
+               '2': '0010', 
+               '3': '0011',
+               '4': '0100',
+               '5': '0101',
+               '6': '0110',
+               '7': '0111',
+               '8': '1000',
+               '9': '1001',
+               'A': '1010',
+               'B': '1011',
+               'C': '1100',
+               'D': '1101',
+               'E': '1110',
+               'F': '1111'}
+        s = ''
+        for letter in code:
+            s += dic[letter]
+        return s[256-k:]
+
+
     n = random.randint(1, 10000000)
-    print(n)
-    x = hashlib.sha256(str(n).encode('utf-8')).hexdigest()
+    x = str(n).encode('utf-8')
+    x_code = hashlib.sha256(x).hexdigest()
+    x_str = helper(x_code)
     n += 1
-    y = hashlib.sha256(str(n).encode('utf-8')).digest()
-    # while x[256-k:] != y[256-k:]:
-    #     n += 1 
-    #     y = hashlib.sha256(n.encode('utf-8'))
-    print(x, y)
-    return( b'\x00',b'\x00' )
+    y = str(n).encode('utf-8')
+    y_code = hashlib.sha256(y).hexdigest()
+    y_str = helper(y_code)
+    while x_str != y_str:
+        n += 1
+        y = str(n).encode('utf-8')
+        y_code = hashlib.sha256(y).hexdigest()
+        y_str = helper(y_code)
+    return( x,y )
 
 
-
-print(hash_collision)
