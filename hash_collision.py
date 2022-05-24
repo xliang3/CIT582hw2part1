b12e6dc9b16e0b5a11037c145b3c3fd3bb3edfc2
Jstar414370153 <45889798+Jstar414370153@users.noreply.github.com>
2022-05-23 19:48:16 -0500

    3rd

--- hash_collision.py
+++ hash_collision.py
@@ -12,15 +12,15 @@ def hash_collision(k):
    
     #Collision finding code goes here
     n = random.randint(1, 10000000)
-    x = hashlib.sha256(n.encode('utf-8'))
-    n += 1
-    y = hashlib.sha256(n.encode('utf-8'))
+    # x = hashlib.sha256(n.encode('utf-8'))
+    # n += 1
+    # y = hashlib.sha256(n.encode('utf-8'))
     # while x[256-k:] != y[256-k:]:
     #     n += 1 
     #     y = hashlib.sha256(n.encode('utf-8'))
-    print(x, y)
-    return( x, y )
+    # print(x, y)
+    return( b'\x00',b'\x00' )
 
 
 
-
+print(hash_collision)
